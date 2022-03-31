522811f5425872f8cf135354fecac1035b021971
潘琪焕 <610100380@qq.com>
2022-03-31 14:43:29 +0800

    签名增加【平台密钥】

--- src/Kernel/Storage/Platform.php
+++ src/Kernel/Storage/Platform.php
@@ -13,14 +13,17 @@ class Platform extends StorageAbstract implements Storage
     {
         $log["method"] = "juli.optimizer.analyse.api";
         $log["platform_id"] = $this->config['platform_id'];
+        $log["platform_secret"] = $this->config['platform_secret'];
         $log["project_id"] = $this->config['project_id'];
         $log["timestamp"] = date('Y-m-d H:i:s');
         $log["sign_type"] = $this->config['sign_type'];
 
-        $signParam = Arr::sortRecursive(Arr::only($log, ['method', 'platform_id', 'project_id', 'timestamp',]));
+        $signParam = Arr::sortRecursive(Arr::only($log, ['method', 'platform_id', 'platform_secret', 'project_id', 'timestamp',]));
         if ($this->config['sign_type'] == 'MD5') {
             $log['sign'] = md5(implode('&', $signParam));
         }
+
+        unset($log['platform_secret']);
 
         $client = new Client([
             'timeout' => $this->config['timeout'] ?? 1,
